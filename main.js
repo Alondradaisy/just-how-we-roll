34d37a1dbbbdef02c838aa5bd906782a6d02c56f
Alondra Warren <daisy.codeimmersives.com>
2021-03-23 06:53:07 -0700

    attempt through confusion

--- main.js
+++ main.js
@@ -31,20 +31,49 @@ const sortByNumber = function(arr) {
  * YOUR CODE BELOW *
  *******************/
 
+  let sixes = document.createElement('img');
+  sixes.src = "images/start/d6.png";
+  document.querySelector('#6d-button').appendChild(sixes);
+
+
+
+  let doubleSixes = document.createElement('img');
+  doubleSixes.src = 'images/start/d6.png' + 'images/start/d6.png';
+  document.querySelector('#double-6d-buttons').appendChild(doubleSixes);
+
+  let twelves = document.createElement('img');
+  twelves.src = 'images/start/d12.jpeg';
+  document.querySelector('#d12-button').appendChild(twelves);
+
+  let twenties = document.createElement('img');
+  twentiesImg.src = 'images/start/d20.jpg';
+  document.querySelector('#d20-button').appendChild(twenties);
 
 
 /*******************
  * EVENT LISTENERS *
  *******************/
+const startingDice = document.querySelector('#d6'); 
+startingDice.addEventListener('click', startingDice);
 
+const startingDblSixes = document.querySelector('#double-d6');
+startingDblSixes.addEventListener('click', startingDblSixes);
 
+const startingTwelves = document.querySelector('#d12');
+startingTwelves.addEventListener('click', startingTwelves);
 
+const startingTwenties = document.querySelector('#d20');
+startingTwenties.addEventListener('click', startingTwenties);
 
 /******************
  * RESET FUNCTION *
  ******************/
 
+let sixes = [];
+const 6RollImg = document.querySelector('#d6-roll');
+6RollImg.addEventListener('click', function() ) {
 
+}
 
 /****************************
  * CLICK HANDLING FUNCTIONS *
